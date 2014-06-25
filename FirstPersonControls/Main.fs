e44990088d50a3611788f2fc517728dd5251928f
Rakonczai S?ndor <rakonczai.sandor@outlook.hu>
2014-06-25 10:42:11 +0200

    Initial commit.

---
+++ FirstPersonControls/Main.fs
@@ -0,0 +1,77 @@
+namespace FirstPersonControls
+
+open IntelliFactory.WebSharper.InterfaceGenerator
+
+module Definition =
+    open IntelliFactory.WebSharper.ThreeJs
+    open IntelliFactory.WebSharper.Dom
+
+    let O = T<unit>
+
+    let mainResource =
+        Resource "FirstPersonControls" "FirstPersonControls.js"
+        |> RequiresExternal [
+            T<IntelliFactory.WebSharper.ThreeJs.Resources.Js>
+        ]
+
+    let FirstPersonControls =
+        Class "THREE.FirstPersonControls"
+        |+> [
+            Constructor (T<THREE.Object3D>?``object`` * !? T<Element>?domElement)
+        ]
+        |+> Protocol [
+            "object"            =@ T<THREE.Object3D>
+            "target"            =@ T<THREE.Vector3>
+            "domElement"        =? T<Element>
+            "movementSpeed"     =@ T<float>
+            "lookSpeed"         =@ T<float>
+            "lookVertical"      =@ T<bool>
+            "autoForward"       =@ T<bool>
+            "activeLook"        =@ T<bool>
+            "heightSpeed"       =@ T<bool>
+            "heightCoef"        =@ T<float>
+            "heightMin"         =@ T<float>
+            "heightMax"         =@ T<float>
+            "constrainVertical" =@ T<bool>
+            "verticalMin"       =@ T<float>
+            "verticalMax"       =@ T<float>
+            "autoSpeedFactor"   =@ T<float>
+            "mouseX"            =? T<int>
+            "mouseY"            =? T<int>
+            "lat"               =@ T<float>
+            "lon"               =@ T<float>
+            "phi"               =@ T<float>
+            "theta"             =@ T<float>
+            "moveForward"       =@ T<bool>
+            "moveBackward"      =@ T<bool>
+            "moveLeft"          =@ T<bool>
+            "moveRight"         =@ T<bool>
+            "freeze"            =@ T<bool>
+            "mouseDragOn"       =@ T<bool>
+            "viewHalfX"         =@ T<float>
+            "viewHalfY"         =@ T<float>
+
+            "update" => T<float>?delta ^-> O
+        ]
+        |> Requires [
+            mainResource
+        ]
+
+    let Assembly =
+        Assembly [
+            Namespace "IntelliFactory.WebSharper.ThreeJs.THREE" [
+                 FirstPersonControls
+            ]
+            Namespace "IntelliFactory.WebSharper.ThreeJs.Resources" [
+                 mainResource
+            ]
+        ]
+
+[<Sealed>]
+type Extension () =
+    interface IExtension with
+        member x.Assembly =
+            Definition.Assembly
+
+[<assembly: Extension(typeof<Extension>)>]
+do ()
---
+++ build.fsx
@@ -0,0 +1,42 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    (
+        BuildTool().PackageId("WebSharper.ThreeJs.FirstPersonControls", "2.5")
+        |> fun bt ->
+            bt.WithFramework bt.Framework.Net40
+    )
+        .References (fun r ->
+            [
+                r.NuGet("WebSharper.ThreeJs").Reference()
+            ]
+        )
+
+let main =
+    (
+        bt.WebSharper.Extension "IntelliFactory.WebSharper.ThreeJs.FirstPersonControls"
+        |> FSharpConfig.BaseDir.Custom "FirstPersonControls"
+    )
+        .SourcesFromProject("FirstPersonControls.fsproj")
+        .Embed [
+            "FirstPersonControls.js"
+        ]
+
+bt.Solution [
+    main
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "WebSharper.ThreeJs.FirstPersonControls"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://bitbucket.org/intellifactory/websharper.threejs.firstpersoncontrols"
+                Description = "WebSharper Extensions for ThreeJs.FirstPersonControls 20140124"
+                Authors = ["IntelliFactory"]
+                RequiresLicenseAcceptance = true
+            }
+        )
+        .Add(main)
+]
+|> bt.Dispatch
