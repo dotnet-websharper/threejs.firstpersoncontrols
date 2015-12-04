17fca77875f4d47f5b66749b9137ce9794bddbcd
Sandor Rakonczai <rakonczai.sandor@outlook.hu>
2015-12-04 18:29:52 +0100

    Add Zafir build scripts

---
+++ build-zafir.fsx
@@ -0,0 +1,36 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.ThreeJs.FirstPersonControls")
+        .VersionFrom("Zafir")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun fw -> fw.Net40)
+
+let main =
+    bt.Zafir.Extension("WebSharper.ThreeJs.FirstPersonControls")
+        .SourcesFromProject()
+        .Embed(["FirstPersonControls.js"])
+        .References(fun r ->
+            [
+                r.NuGet("Zafir.ThreeJs").ForceFoundVersion().Reference()
+            ]
+        )
+
+bt.Solution [
+    main
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "Zafir.ThreeJs.FirstPersonControls"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://bitbucket.org/intellifactory/websharper.threejs.firstpersoncontrols"
+                Description = "WebSharper Extensions for ThreeJs.FirstPersonControls 20140124"
+                Authors = ["IntelliFactory"]
+                RequiresLicenseAcceptance = true
+            }
+        )
+        .Add(main)
+]
+|> bt.Dispatch
